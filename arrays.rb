95786a3261fe53bd429d944775e8b8ac012327a3
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-05-16 19:48:03 -0500

    Input array testing

--- arrays.rb
+++ arrays.rb
@@ -15,3 +15,22 @@ accounts = [400.30, 50.50, 25.43, 5000.20]
 
 feelings = [happy = true, sad = false, angry = true, sick = true]
 
+#1. Calling the friend variable, will print the coorisponding 
+#name in order from 0 - 3  -- "Travis"
+
+puts friends [0]
+
+#2. calling the ages variable will print the age of the 
+#coorisponding number in parenthesis "35"
+
+puts ages[3]
+
+#3. calling he accounts variable will print the float number
+#coorisponding to the numbered order "5000.20"
+
+puts accounts [3]
+
+#4 callin the feelings variable will populate with the
+#coorisponding boolean set for each variable "true"
+
+puts feelings [2]
