263cde806000d5654c3ea7d212bf890326ff6711
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-05-16 19:42:01 -0500

    Update array variables

---
+++ arrays.rb
@@ -0,0 +1,17 @@
+#Declare a variable  that stores an Array of at least 4 
+#strings
+
+friends = ["Travis", "Ron", "Lindsey", "Mallory"]
+
+#Declare a variable that stores an Array of at least 4 integers
+
+ages = [43, 37, 36, 35]
+
+#Declare a variable that stores an array of at least 4 floats
+
+accounts = [400.30, 50.50, 25.43, 5000.20]
+
+#Declare a variable that stores an Array of at least 4 Booleans
+
+feelings = [happy = true, sad = false, angry = true, sick = true]
+
