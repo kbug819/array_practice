71fafe4f53bfb5e2b1c1896107c055d1e794b381
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-05-22 22:14:16 -0500

    Update with corrections from feedback

--- arrays.rb
+++ arrays.rb
@@ -13,12 +13,12 @@ accounts = [400.30, 50.50, 25.43, 5000.20]
 
 #Declare a variable that stores an Array of at least 4 Booleans
 
-feelings = [happy = true, sad = false, angry = true, sick = true]
+feelings = [true, false, true, true]
 
 #1. Calling the friend variable, will print the coorisponding 
 #name in order from 0 - 3  -- "Travis"
 
-puts friends [0]
+puts friends[0]
 
 #2. calling the ages variable will print the age of the 
 #coorisponding number in parenthesis "35"
@@ -28,12 +28,12 @@ puts ages[3]
 #3. calling he accounts variable will print the float number
 #coorisponding to the numbered order "5000.20"
 
-puts accounts [3]
+puts accounts[3]
 
 #4 callin the feelings variable will populate with the
 #coorisponding boolean set for each variable "true"
 
-puts feelings [2]
+puts feelings[2]
 
 ##Demonstate your knowledge of index positions:
 #Index positions
